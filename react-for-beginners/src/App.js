0d71237a9f542d0e6079d954461d4ded940bb06b
suncream100 <murmuring91@gmail.com>
2023-01-21 23:06:21 +0900

    add: chapter7 (Coin Tracker)

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,51 +1,28 @@
 import { useState, useEffect } from "react";
 
-// 예시 2.
-function Hello() {
-  useEffect(() => {
-    console.log("created :)");
-    return () => console.log("destroyed :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+// Coin Tracker
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []) // 빈 배열이면 한번만 실행
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>The Coins! {loading ? null : `Top${coins.length}`}</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((item, index) => (
+          <li key={item.id}>{index+1}. {item.name} ({item.symbol}): ${item.quotes.USD.price.toFixed(4)}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-// 예시 1.
-// function App() {
-//   const [counter, setValue] = useState(0);
-//   const [keyword, setKeyword] = useState("");
-//   const onClick = () => setValue((prev) => prev + 1);
-//   const onChange = (event) => setKeyword(event.target.value);
-//   console.log("i run all the time");
-//   useEffect(() => {
-//     console.log("CALL THE API...");
-//   }, []);
-//   useEffect(() => {
-//     if (keyword !== "" && keyword.length > 5) {
-//       console.log("search for", keyword);
-//     }
-//   }, [keyword]);
-//   return (
-//     <div>
-//       <input
-//         onChange={onChange}
-//         type="text"
-//         placeholder="Search here..."
-//         value={keyword}
-//       />
-//       <h1>{counter}</h1>
-//       <button onClick={onClick}>click me</button>
-//     </div>
-//   );
-// }
 
 export default App;
