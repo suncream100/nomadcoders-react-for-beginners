ce1d04aee619a4c3265336605cc18488d77c7e75
suncream100 <murmuring91@gmail.com>
2023-01-30 18:02:41 +0900

    update: chapter7 (Movie App) - detail update

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,22 +1,56 @@
-import {useEffect} from "react";
+import {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import {useParams} from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [detail, setDetail] = useState([]);
   const {id} = useParams();
   const getMovie = async() => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     console.log(json);
+    setDetail(json.data.movie);
+    setLoading(false);
   }
   useEffect(() => {
     getMovie();
   }, []);
   return (
-    <div>
+    <div className={styles.wrap_detail}>
       <Link to="/">Move Home</Link>
-      <h1>Detail</h1>
+      {loading ? (
+        <h1>Loading</h1> 
+      ) : (
+        <div className={styles.box_detail}>
+          <h1>{detail.title}</h1>
+          <div><img src={detail.medium_cover_image} alt={`${detail.title} Poster`} /></div>
+          <dl>
+            <div>
+              <dt>Year</dt>
+              <dd>{detail.year}</dd>
+            </div>
+            <div>
+              <dt>Genres</dt>
+              <dd>
+                {detail.genres.map((item) => (
+                  <span key={item}>{item}</span>
+                ))}
+              </dd>
+            </div>
+            <div>
+              <dt>Rating</dt>
+              <dd>{detail.rating}</dd>
+            </div>
+            <div>
+              <dt>Description</dt>
+              <dd>{detail.description_full}</dd>
+            </div>
+          </dl>
+        </div>
+      )}
     </div>
   );
 }
